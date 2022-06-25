ac1a3ff8eac47734b5a085eebf98ee13430c486a
Olukeye <seunolukeye2000@gmail.com>
2022-06-25 22:40:08 +0100

    add likes models

--- app/models.py
+++ app/models.py
@@ -23,3 +23,10 @@ class User(Base):
     email = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+
+class Like(Base):
+    __tablename__ = "likes"
+    post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
+    
