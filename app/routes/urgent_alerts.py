f5cb3172bb3ad3006afd9197368b5916b9acc320
legendkartik45 <kartik.khuda.45@gmail.com>
2022-10-28 19:12:45 +0530

    ..

--- app/routes/urgent_alerts.py
+++ app/routes/urgent_alerts.py
@@ -1,7 +1,7 @@
 from operator import ge
 from fastapi import FastAPI, Response, requests, status, HTTPException, Depends, APIRouter, File, UploadFile
 from sqlalchemy.orm import Session
-from app.models.urgent_alerts import create_response, get_others_urgent_alert, get_response
+from app.models.urgent_alerts import create_response, get_others_urgent_alert, get_response, get_tokens_of_neighbour
 from app.models.user import neighbour_user
 from ..routes.notifications import is_notification_enable, is_preview_message_enable, create_device_token, get_user_device_token
 from ..utils import  push_notification
@@ -13,7 +13,7 @@ from ..pydantic_schemas.posts import Post, PostAll, PostOpt, CreatePost, Likes
 from sqlalchemy import func
 from ..database import get_db
 from ..modules.users.userRepository import register_new, singleUser, updateUser
-from ..modules.urgent_alerts.alertsRepository import create_alert, myalert, update_alert, get_alert_of_others, delete_alert,get_urgent_alerts_by_id,get_total_urgent_alerts
+from ..modules.urgent_alerts.alertsRepository import create_alert ,myalert, update_alert, get_alert_of_others, delete_alert,get_urgent_alerts_by_id,get_total_urgent_alerts
 from ..pydantic_schemas.user import CreateUser, UserOpt,  User, UserUpdate
 import os
 
@@ -57,7 +57,5 @@ async def respond_to_urgent_alerts(id:int, db:Session= Depends(get_db), user: in
 
 @router.get("/urgent-alerts/{id}/respond")
 async def get_response_of_urgent_alerts(id:int, db:Session= Depends(get_db), user: int= Depends(oauth2.oauth2.get_current_user)):
-    tokenn=get_user_device_token(db=db)
-    send_notification_to_neighbours(user=user, db=db)
     return get_response(user=user, db=db)
     
