3f5ef8a536ec4999ecdb38f34222de6cebdc3795
yash joshi <yjoshi5107@gmail.com>
2022-11-25 20:42:38 +0530

    .

--- app/main.py
+++ app/main.py
@@ -1,12 +1,26 @@
 from fastapi import FastAPI
 from app.api.routes import user,post,auth,votes,comments,events,search,admin,profiles,imagesupload,notifications,urgentalerts
 from app.api.database import engine, Base,database
-
+from fastapi.middleware.cors import CORSMiddleware
 
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:5000",
+    "http://admin.padosii.com/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
  
 @app.on_event("startup")  # datbase connection
 async def startup():
